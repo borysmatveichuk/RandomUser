b335e5b0a833a034aefc3d32477746eef5b357bf
unknown <boris.matveychuck@gmail.com>
2017-07-04 00:37:07 +0300

    - custom date format for gson

--- app/src/main/java/net/borkiss/randomuser/service/ServiceGenerator.java
+++ app/src/main/java/net/borkiss/randomuser/service/ServiceGenerator.java
@@ -1,5 +1,8 @@
 package net.borkiss.randomuser.service;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -7,9 +10,15 @@ public class ServiceGenerator {
     private static final String BASE_URL = "https://randomuser.me/api/";
     private static final String TAG = ServiceGenerator.class.getSimpleName();
 
+    private static Gson gson = new GsonBuilder()
+            .setDateFormat("yyyy-mm-dd HH:mm:ss")
+            .setLenient()
+            .create();
+
+
     private static Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build();
 
     public static <T> T createService(Class<T> serviceClass) {
