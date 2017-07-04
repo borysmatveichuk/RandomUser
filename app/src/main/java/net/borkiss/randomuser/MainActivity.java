9056323b2ebe0acef445ee728cc199d4d7b28374
boris <mobile.step2love@gmail.com>
2017-07-04 17:35:18 +0300

    - add recycler view, change theme

--- app/src/main/java/net/borkiss/randomuser/MainActivity.java
+++ app/src/main/java/net/borkiss/randomuser/MainActivity.java
@@ -1,49 +1,53 @@
 package net.borkiss.randomuser;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
+import net.borkiss.randomuser.adapter.UsersAdapter;
 import net.borkiss.randomuser.data.UserDataSource;
 import net.borkiss.randomuser.data.UserRepository;
 import net.borkiss.randomuser.data.model.User;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private UserRepository userRepository;
-    private TextView textTest;
+    private RecyclerView recyclerView;
+    private UsersAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         userRepository = UserRepository.getInstance();
         setContentView(R.layout.activity_main);
-        textTest = (TextView) findViewById(R.id.textTest);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setupToolbar();
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new UsersAdapter(new ArrayList<User>(), this);
+        recyclerView.setAdapter(adapter);
+
+        RecyclerView.ItemDecoration dividerItemDecoration = new DividerItemDecoration(recyclerView.getContext(),
+                DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+
 
         userRepository.getAllUsers(new UserDataSource.LoadUsersCallback() {
             @Override
             public void onUsersLoaded(List<User> users) {
-                final StringBuilder s = new StringBuilder();
-
-                for (User user : users) {
-                    s.append(user.getFirstName())
-                            .append(" ")
-                            .append(user.getLastName())
-                            .append(" ")
-                            .append(user.getCity())
-                            .append(" ")
-                            .append(user.getDateOfBirth())
-                            .append("\n");
-                }
-
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        textTest.setText(s.toString());
-                    }
-                });
+                adapter.update(users);
             }
 
             @Override
@@ -51,10 +55,15 @@ public class MainActivity extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        textTest.setText("Data not available");
                     }
                 });
             }
         });
+    }
+
+    private void setupToolbar() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle(getResources().getString(R.string.app_name));
+        }
     }
 }
